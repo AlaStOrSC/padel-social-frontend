59c870a049411bd56c5080d322262f7012c78705
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-07 01:13:18 +0200

    preparando para render

--- backend/src/config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoUri: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
-  emailUser: process.env.EMAIL_USER,
-  emailPass: process.env.EMAIL_PASS,
-  port: process.env.PORT || 3000,
-};
--- backend/src/routes/fileRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadProfilePicture } = require('../controllers/fileController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const { upload } = require('../services/fileService');
-
-router.post('/upload-profile-picture', authMiddleware(), upload, uploadProfilePicture);
-
-module.exports = router;
