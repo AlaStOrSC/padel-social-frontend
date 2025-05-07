572ce2ec9e0ed0153833638c13acbd703477c78e
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-07 03:29:45 +0200

    fixeando problemas con las importaciones

--- src/scripts/gestor.js
+++ src/scripts/gestor.js
@@ -9,10 +9,10 @@ import {
   updateMatch,
   deleteMatch,
   logout,
-} from './api.js';
-import { getWeather } from './api/weather.js';
-import { checkAuth } from './utils.js';
-import {fetchUserProfile} from './api.js'
+} from '/src/scripts/api.js';
+import { getWeather } from '/src/scripts/api/weather.js';
+import { checkAuth } from '/src/scripts/utils.js';
+import {fetchUserProfile} from '/src/scripts/api.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
   moment.locale('es');
