29ec47a09ae848b9e516833b4380cd28df0dea2e
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-12 13:23:35 +0200

    cambio para si no hay solicitudes de amistad

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -32,7 +32,18 @@ const renderPendingRequestCount = async (welcomeText, userName) => {
 
       const welcomeMessageDiv = welcomeText.parentElement;
       welcomeMessageDiv.appendChild(pendingMessage);
+    } else if (pendingCount === 0) {
+      const pendingMessage = document.createElement('p');
+      pendingMessage.id = 'pending-message';
+      pendingMessage.className = 'pending-message';
+      pendingMessage.textContent = `No tienes nuevas solicitudes de amistad`;
+      pendingMessage.style.color = 'red';
+      pendingMessage.style.marginTop = '10px';
+
+      const welcomeMessageDiv = welcomeText.parentElement;
+      welcomeMessageDiv.appendChild(pendingMessage);
     }
+
   } catch (error) {
     console.error('Error al obtener el conteo de solicitudes pendientes:', error);
   }
