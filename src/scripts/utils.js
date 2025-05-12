644c3302fc4146a32e6e2c49300640649bcfe94d
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-12 19:18:21 +0200

    colorfix

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -31,12 +31,12 @@ const getMessageConfig = (count) => {
   if (count === 0) {
     return {
       text: 'No tienes nuevas solicitudes de amistad',
-      color: '#007BFF',
+      color: '#4657f3',
     };
   }
   return {
     text: `Tienes ${count} solicitud${count === 1 ? '' : 'es'} de amistad nueva${count === 1 ? '' : 's'}`,
-    color: '#007BFF',
+    color: '#4657f3',
   };
 };
 
